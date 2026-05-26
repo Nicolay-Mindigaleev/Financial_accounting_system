2beb906f08a58a745ffa2dc3579132e8fd648c49
Николай <mindigaleevkola@gmail.com>
2026-05-26 22:29:52 +0700

    Fix registration

--- core/views.py
+++ core/views.py
@@ -2,19 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-
-@login_required
-def index(request):
-    return render(request, "core/index.html")
+from .models import Transaction
+from .forms import UserDataCreationForm
+#registration or auntification
 
 def register_view(request):
-    #form = UserCreationForm(request.POST)
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserDataCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect("index")
-    form = UserCreationForm()
+    form = UserDataCreationForm()
     return render(request, "core/registration.html",  {'form': form})
 
 def login_view(request):
@@ -29,4 +27,13 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    return redirect("index")
+
+#main functional
+
+@login_required
+def index(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, "core/index.html", {'transactions': transactions})
+def add_transaction(request):
     return redirect("index")
